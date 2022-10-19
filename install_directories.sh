cd5e88412843d104c5c4de21aa3c5a09c98e80e9
Jay20Games <50749814+Jay20Games@users.noreply.github.com>
2022-10-19 16:12:10 -0700

    Update install_directories.sh

--- install_directories.sh
+++ install_directories.sh
@@ -1,9 +1,8 @@
 mkdir /data
-mkdir /data/{torrents|usenet|media|configs}
-mkdir /data/torrents/{tv|movies|music}
-mkdir /data/usenet/{tv|movies|music}
-mkdir /data/media/{tv|movies|music}
-mkdir /data/configs
+mkdir /data/{torrents,usenet,media,configs}
+mkdir /data/torrents/{tv,movies,music}
+mkdir /data/usenet/{tv,movies,music}
+mkdir /data/media/{tv,movies,music}
 
 sudo chown -R $USER:$USER /data
 sudo chmod -R a=,a+rX,u+w,g+w /data
