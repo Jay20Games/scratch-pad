4eaf8811da341903e883aa55b5a17f010559c643
Jay20Games <50749814+Jay20Games@users.noreply.github.com>
2022-10-19 15:54:57 -0700

    add configs dir

--- install_directories.sh
+++ install_directories.sh
@@ -1,6 +1,7 @@
 mkdir /data/torrents/{tv|movies|music}
 mkdir /data/usenet/{tv|movies|music}
 mkdir /data/media/{tv|movies|music}
+mkdir /data/configs
 
 sudo chown -R $USER:$USER /data
 sudo chmod -R a=,a+rX,u+w,g+w /data
