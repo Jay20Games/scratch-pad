8eebdf8d61d39468ada297a8a6f8594bcd4b2e32
Jay20Games <50749814+Jay20Games@users.noreply.github.com>
2022-11-10 21:28:21 -0800

    Create install_docker_portainer.sh

---
+++ install_docker_portainer.sh
@@ -0,0 +1,5 @@
+apt update && sudo apt upgrade -y
+apt install docker.com -y
+apt install docker-compose
+docker volume create portainer_data
+docker run -d -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
